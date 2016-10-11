d198413c29dfe8de19de99aaf74691576df1638c
dom133 <dom133@users.noreply.github.com>
2016-10-11 16:15:27 +0200

    Update mysql-polaczenie.php

--- Nowa Strona/mysql-polaczenie.php
+++ Nowa Strona/mysql-polaczenie.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect ("localhost", "skymin_strona", "Pawelek1") or
+	$conn = mysqli_connect ("localhost", "skymin_strona", "") or
 	die ("Blad podczas polaczenia z MySQL. Jesli mozesz to poinformuj o tym administracje. Sprobuj takze odswiezyc strone.");
 	mysqli_select_db ($conn, "skymin_strona") or
 	die ("Blad podczas wybierania bazy. Jesli mozesz to poinformuj o tym administracje. Sprobuj takze odswiezyc strone.");
